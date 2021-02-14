a000e11d775e6c83f744083a2386575603e01a03
Fazel Saeedi <fazels36@gmail.com>
2021-02-15 01:58:24 +0330

    develop findSearchCOntent for Select Custom element in WebDriver

--- browser.py
+++ browser.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-
+import time
 
 class Browser:
 
@@ -28,3 +28,38 @@ class Browser:
         browser = self.__browser
         browser.get(self.__searchEngin_Url)
         print("Get" + '  ->  ' + str(self.__searchEngin_Url))
+
+
+
+
+    def findSearchContent(self):
+
+        """
+
+        |--------------------------------------------------
+        |                                                 |
+        |   Enter  __searchContent in input               |
+        |                              and Click it       |
+        |                                                 |
+        |--------------------------------------------------
+        |                                                 |
+        |   1 - use  find_element_by_name -> Input        |
+        |            and write __searchContent            |
+        |                                                 |
+        |                                                 |
+        |   2 - use find_element_by_class_name -> Submit  |
+        |            and click                            |
+        |                                                 |
+        ---------------------------------------------------
+
+        """
+
+        # ---  1
+        browser = self.__browser
+        browser.find_element_by_name('q').send_keys(self.__searchContent)
+        time.sleep(3)
+
+
+        # ---  2
+        browser.find_element_by_class_name('gNO89b').click()
+        print("Get" + '  ->  ' + str(self.__searchEngin_Url) + '  ->  ' + 'Search ' + '  ->  ' + str(self.__searchContent))
